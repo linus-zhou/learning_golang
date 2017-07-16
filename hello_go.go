41cc43fdce1d7e844a14183a95bfb4865e1f5d02
linus.zhou <342174066@qq.com>
2017-07-16 18:57:16 +0800

    添加注释

--- hello_go.go
+++ hello_go.go
@@ -1,7 +1,8 @@
-package main
 
-import "fmt"
+package main //一定要引入main包，否则无法执行
+
+import "fmt" //引入包
 
 func main() {
-    fmt.Println("Hello, go!!!")
+    fmt.Println("Hello, go!!!") //输出字符串，加换行
 }
