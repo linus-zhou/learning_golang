665282f8519b0a789566dbc002fec9cac5fbe7c4
linus.zhou <342174066@qq.com>
2017-07-24 23:51:20 +0800

    好吧 go

---
+++ map-literals.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct{
+	Lot, Long float64
+}
+
+var m = map[string]Vertex{ //map的使用必须要有键值
+	"zhou" : Vertex{
+		30.121234,34.566787,
+	},
+	"zhou1" : Vertex{
+		56.12344,78.23456,
+	},
+}
+
+func main(){
+	fmt.Println(m)
+}
---
+++ maps.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Vertex struct {  //定义结构
+	Lat, Long float64
+}
+var m map[int8]Vertex //定义一个map,[]里面表示其键值类型
+//var m map[string]Vertex //定义一个map,[]里面表示其键值类型
+
+func main() {
+	m = make(map[int8]Vertex) //map必须使用make函数生成
+	//m = make(map[string]Vertex) //map必须使用make函数生成
+
+	m[1] = Vertex{
+		40.23234, -63.124343,
+	}
+
+	fmt.Println(m[1])
+}
